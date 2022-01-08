1734b339563129e64be3ada76766b6f5467375c7
harrylioutaiwan <pjart11th27@gmail.com>
2022-01-08 14:57:15 +0800

    callback1.js update

--- callback/callback1.js
+++ callback/callback1.js
@@ -1,24 +1,30 @@
-let doWork = function(job, timer, callback){
-    setTimeout(()=>{
-        //callback的設計上
-        //通常第一個參數是錯誤
-        //通常第二個參數會是結果
-        callback(null, `完成工作 ${job}`);
-    }, timer);
+let doWork = function (job, timer, callback) {
+  setTimeout(() => {
+    //callback的設計上
+    //通常第一個參數是錯誤
+    //通常第二個參數會是結果
+    callback(null, `完成工作 ${job}`);
+  }, timer);
 };
 
-//刷牙  --> 吃早餐 --> 做功課
+// 刷牙 --> 吃早餐 --> 寫功課
 let dt = new Date();
-console.log(`Start ${dt.toISOString()}`)
-
+console.log(`Start ${dt.toISOString()}`);
 doWork("刷牙", 2000, (err, result) => {
-    console.log(result)
-}) 
+  // 刷牙 --> 吃早餐 --> 寫功課
+  let dt = new Date();
+  console.log(`${result} at ${dt.toISOString()}`);
+  
+});
 
-doWork("吃早餐", 3000, (err, result) => {
-    console.log(result)
-}) 
+doWork("吃早餐", 5000, (err, result) => {
+  let dt = new Date();
+  console.log(`${result} at ${dt.toISOString()}`);
+  
+});
 
-doWork("寫功課", 2000, (err, result) => {
-    console.log(result)
-}) 
+doWork("寫功課", 7000, (err, result) => {
+  let dt = new Date();
+  console.log(`${result} at ${dt.toISOString()}`);
+  
+});
