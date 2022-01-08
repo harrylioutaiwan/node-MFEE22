95dd6b277b822054855fd183c8c5578e519c9755
harrylioutaiwan <pjart11th27@gmail.com>
2022-01-08 15:06:34 +0800

    callback1.js update

--- callback/callback1.js
+++ callback/callback1.js
@@ -15,16 +15,18 @@ doWork("刷牙", 2000, (err, result) => {
   let dt = new Date();
   console.log(`${result} at ${dt.toISOString()}`);
   
+  doWork("吃早餐", 3000, (err, result) => {
+    let dt = new Date();
+    console.log(`${result} at ${dt.toISOString()}`);
+    
+    doWork("寫功課", 2000, (err, result) => {
+        let dt = new Date();
+        console.log(`${result} at ${dt.toISOString()}`);
+        
+      });
+  });
 });
 
-doWork("吃早餐", 5000, (err, result) => {
-  let dt = new Date();
-  console.log(`${result} at ${dt.toISOString()}`);
-  
-});
 
-doWork("寫功課", 7000, (err, result) => {
-  let dt = new Date();
-  console.log(`${result} at ${dt.toISOString()}`);
-  
-});
+
+
